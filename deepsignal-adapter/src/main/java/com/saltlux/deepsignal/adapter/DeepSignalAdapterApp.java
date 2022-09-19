f85cebe73a157901e2efd886e5415f65e1325fd8
ThienHa <thienhv@saltlux.com>
2022-09-19 14:01:41 +0700

    chore: update configuration to support refreshable

--- deepsignal-adapter/src/main/java/com/saltlux/deepsignal/adapter/DeepSignalAdapterApp.java
+++ deepsignal-adapter/src/main/java/com/saltlux/deepsignal/adapter/DeepSignalAdapterApp.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
